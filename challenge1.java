e21036e6ba56827ae99f65ae96ca1518afc7d460
Inal <inalzaelani2806@gmail.com>
2023-02-08 13:12:55 +0700

    First Commit

---
+++ challenge1.java
@@ -0,0 +1,18 @@
+public class challenge1 {
+            public static void main(String[] args) {
+
+                int a = 15;
+                int b = 20;
+                int c= a;
+                /* lengkapi code disini*/
+                a=b;
+                b=c;
+
+
+
+                System.out.printf("%d - %d", a, b);
+
+            }
+
+        }
+
